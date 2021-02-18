685def71c3c80e3d5ee672bc8b8634e59dc8d228
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-02-18 22:39:06 +0200

    file edit

---
+++ js/app.js
@@ -0,0 +1,151 @@
+'use strict'
+
+let cb1 = document.getElementById('cb1');
+cb1.addEventListener('change',deletItem1)
+function deletItem1(event){
+    if (event.target.checked) {
+        let p1 = document.getElementById('p1');
+        p1.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p1 = document.getElementById('p1');
+        p1.removeAttribute('style');
+      }
+}
+
+let cb2 = document.getElementById('cb2');
+cb2.addEventListener('change',deletItem2)
+function deletItem2(event){
+    if (event.target.checked) {
+        let p2 = document.getElementById('p2');
+        p2.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p2 = document.getElementById('p2');
+        p2.removeAttribute('style');
+      }
+}
+
+let cb3 = document.getElementById('cb3');
+cb3.addEventListener('change',deletItem3)
+function deletItem3(event){
+    if (event.target.checked) {
+        let p3 = document.getElementById('p3');
+        p3.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p3 = document.getElementById('p3');
+        p3.removeAttribute('style');
+      }
+}
+
+let cb4 = document.getElementById('cb4');
+cb4.addEventListener('change',deletItem4)
+function deletItem4(event){
+    if (event.target.checked) {
+        let p4 = document.getElementById('p4');
+        p4.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p4 = document.getElementById('p4');
+        p4.removeAttribute('style');
+      }
+}
+
+let cb5 = document.getElementById('cb5');
+cb5.addEventListener('change',deletItem5)
+function deletItem5(event){
+    if (event.target.checked) {
+        let p5 = document.getElementById('p5');
+        p5.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p5 = document.getElementById('p5');
+        p5.removeAttribute('style');
+      }
+}
+
+let cb6 = document.getElementById('cb6');
+cb6.addEventListener('change',deletItem6)
+function deletItem6(event){
+    if (event.target.checked) {
+        let p6 = document.getElementById('p6');
+        p6.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p6 = document.getElementById('p6');
+        p6.removeAttribute('style');
+      }
+}
+
+let cb7 = document.getElementById('cb7');
+cb7.addEventListener('change',deletItem7)
+function deletItem7(event){
+    if (event.target.checked) {
+        let p7 = document.getElementById('p7');
+        p7.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p7 = document.getElementById('p7');
+        p7.removeAttribute('style');
+      }
+}
+
+let cb8 = document.getElementById('cb8').checked = true;
+cb8 = document.getElementById('cb8');
+let p8 = document.getElementById('p8');
+p8.setAttribute('style','text-decoration: line-through')
+cb8.addEventListener('change',deletItem8)
+function deletItem8(event){
+    if (event.target.checked) {
+        let p8 = document.getElementById('p8');
+        p8.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p8 = document.getElementById('p8');
+        p8.removeAttribute('style');
+      }
+}
+
+let cb9 = document.getElementById('cb9').checked = true;
+cb9 = document.getElementById('cb9');
+let p9 = document.getElementById('p9');
+p9.setAttribute('style','text-decoration: line-through')
+cb9.addEventListener('change',deletItem9)
+function deletItem9(event){
+    if (event.target.checked) {
+        let p9 = document.getElementById('p9');
+        p9.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p9 = document.getElementById('p9');
+        p9.removeAttribute('style');
+      }
+}
+
+let cb10 = document.getElementById('cb10');
+cb10.addEventListener('change',deletItem10)
+function deletItem10(event){
+    if (event.target.checked) {
+        let p10 = document.getElementById('p10');
+        p10.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p10 = document.getElementById('p10');
+        p10.removeAttribute('style');
+      }
+}
+
+let cb11 = document.getElementById('cb11');
+cb11.addEventListener('change',deletItem11)
+function deletItem11(event){
+    if (event.target.checked) {
+        let p11 = document.getElementById('p11');
+        p11.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p11 = document.getElementById('p11');
+        p11.removeAttribute('style');
+      }
+}
+
+let cb12 = document.getElementById('cb12');
+cb12.addEventListener('change',deletItem12)
+function deletItem12(event){
+    if (event.target.checked) {
+        let p12 = document.getElementById('p12');
+        p12.setAttribute('style','text-decoration: line-through')
+      } else {
+        let p12 = document.getElementById('p12');
+        p12.removeAttribute('style');
+      }
+}
